71001ec8bc225dd0201e10383182afb3da7369e5
Gabriella Marfani <gm518@ic.ac.uk>
2020-10-15 16:13:20 +0100

    Gabriella, Theo: working drawing on canvas but v slow with drawline seperate function from handle event

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
 import Graphics.Blank
 import Data.Text
 
-main :: IO ()
-main = blankCanvas 3000 { events = [pack "mousedown"] } $ \context -> loop context []
+type Coord = (Double, Double)
 
-loop :: DeviceContext -> [(Double, Double)] -> IO ()
-loop context [] = do 
-    event <- wait context
-    case ePageXY event of
-        Nothing -> loop context []
-        Just (x',y') -> loop context ((x' ,y'):[])
-loop context coords@((x,y):_) = do 
-    send context $ do
-        let (w,h) = (width context, height context)
-        let minwh = min w h
+type Line = [Coord]
+
+data MouseState = MouseUp | MouseDown deriving Eq
+
+data CanvasState = CanvasState [Line] MouseState
+
+main :: IO ()
+main = blankCanvas 3000 { events = ["mousedown", "mouseup", "mousemove"] } $ \context -> loop context (CanvasState [[]] MouseUp)
+
+loop :: DeviceContext -> CanvasState -> IO ()
+loop context state@(CanvasState (l:ls) mouseState) = do 
+    send context $ do 
         save()
-        translate (x,y)
-        beginPath()
-        arc(0, 0, minwh * 0.1, 0, 2 * pi, False)
-        lineWidth 10
-        strokeStyle (pack "#00a000")
-        stroke()
+        drawLine context l 
         restore()
+    state' <- handleEvent context state
+    loop context state'
+
+handleEvent :: DeviceContext -> CanvasState -> IO CanvasState
+handleEvent context state@(CanvasState (l:ls) mouseState)= do 
     event <- wait context
-    case ePageXY event of
-        Nothing -> loop context coords
-        Just (x',y') -> loop context ((x' ,y'):coords)
+    return $ case (eType event, ePageXY event) of 
+        ("mousedown", Just (x,y)) -> CanvasState (((x,y):l):ls) MouseDown
+        ("mouseup", Just (x,y)) -> CanvasState ([]:l:ls) MouseUp
+        ("mousemove", Just (x,y)) -> if (mouseState == MouseUp) 
+                                        then state 
+                                        else (CanvasState (((x,y):l):ls) mouseState)
+        _ -> state
+
+
+drawLine :: DeviceContext -> [Coord] -> Canvas ()
+drawLine context [] = mempty
+drawLine context ((x,y):cs) = do 
+    moveTo (x,y)
+    drawLine' context cs 
+
+drawLine' :: DeviceContext -> [Coord] -> Canvas ()
+drawLine' context [] = mempty
+drawLine' context line@((x,y):cs) = do 
+    lineTo(x,y)
+    lineWidth 10
+    strokeStyle "red"
+    stroke() 
+    drawLine context line
 
 
